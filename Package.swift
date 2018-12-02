ccf5db4efc0521aef748102e41b99bcf6f112450
Martial Maillot <martial@h2g.io>
2018-12-02 21:49:17 +0100

    Add vapor as dependency

--- Package.swift
+++ Package.swift
@@ -7,22 +7,15 @@ let package = Package(
     name: "Gitlab",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Gitlab",
-            targets: ["Gitlab"]),
+        .library(name: "Gitlab", targets: ["Gitlab"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Gitlab",
-            dependencies: []),
-        .testTarget(
-            name: "GitlabTests",
-            dependencies: ["Gitlab"]),
+        .target(name: "Gitlab", dependencies: ["Vapor"]),
+        .testTarget(name: "GitlabTests", dependencies: ["Vapor", "Gitlab"]),
     ]
 )
